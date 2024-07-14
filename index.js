288c58218d74b199bff2e5247f8b828c96132445
Shwetank Tripathi <123956154+Shwetank-Tripathi@users.noreply.github.com>
2024-07-14 19:31:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,55 +1,23 @@
 document.querySelector("button").addEventListener("click",myfunction);
 
 
-//Player 1 Dicee
+
 function myfunction(){
+
+//Player 1 Dicee
     var randomNumber1 = Math.random();
     randomNumber1=Math.floor(randomNumber1*6)+1;
-    switch(randomNumber1){
-        case 1: 
-            document.querySelector(".img1").setAttribute("src" , "./images/dice1.png");
-            break;
-        case 2:    
-            document.querySelector(".img1").setAttribute("src" , "./images/dice2.png");
-            break;
-        case 3: 
-            document.querySelector(".img1").setAttribute("src" , "./images/dice3.png");
-            break;
-        case 4: 
-            document.querySelector(".img1").setAttribute("src" , "./images/dice4.png");
-            break;
-        case 5: 
-            document.querySelector(".img1").setAttribute("src" , "./images/dice5.png");
-            break;
-        case 6: 
-            document.querySelector(".img1").setAttribute("src" , "./images/dice6.png");
-            break;
-    }
+    var randomSrc1 = "./images/dice"+randomNumber1+".png";
+    document.querySelector(".img1").setAttributes("src" , randomSrc1);
 
 // Player 2 dicee
 
     var randomNumber2 = Math.random();
     randomNumber2=Math.floor(randomNumber2*6)+1;
-    switch(randomNumber2){
-        case 1: 
-            document.querySelector(".img2").setAttribute("src" , "./images/dice1.png");
-            break;
-        case 2:    
-            document.querySelector(".img2").setAttribute("src" , "./images/dice2.png");
-            break;
-        case 3: 
-            document.querySelector(".img2").setAttribute("src" , "./images/dice3.png");
-            break;
-        case 4: 
-            document.querySelector(".img2").setAttribute("src" , "./images/dice4.png");
-            break;
-        case 5: 
-            document.querySelector(".img2").setAttribute("src" , "./images/dice5.png");
-            break;
-        case 6: 
-            document.querySelector(".img2").setAttribute("src" , "./images/dice6.png");
-            break;
-    }
+    var randomSrc2 = "./images/dice"+randomNumber2+".png";
+    document.querySelector(".img2").setAttributes("src" , randomSrc2);
+
+// Winner
 
     if (randomNumber1>randomNumber2){
         document.querySelector("h2").textContent = "🚩Player 1 Wins!";
